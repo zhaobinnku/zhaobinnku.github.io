15ca8c409630145d52364532ad46347ed29443a6
B Zhao <zhaobinnku@mail.nankai.edu.cn>
2025-03-08 02:29:39 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards','Researches']
+const section_names = ['home', 'publications', 'awards','researches']
 
 
 window.addEventListener('DOMContentLoaded', event => {
