b8b6892cf287f386b33dafb87125a8e4a0bab292
B Zhao <zhaobinnku@mail.nankai.edu.cn>
2025-03-13 23:34:31 +0800

    1

---
+++ contents/group.md
@@ -0,0 +1,4 @@
+- <strong>Master Students</strong>
+  **Chuan Xiong**, GUET, Sep. 2022 -
+  **Shougan Teng**, GUET, Sep. 2023 -
+  **Zhiyuan Liu**, GUET, Sep. 2024 -
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards','researches']
+const section_names = ['home', 'publications','group','alumni', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
